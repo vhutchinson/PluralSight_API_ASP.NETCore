ddc34c90369a28550f027392ddc2c2e0529bf230
vhutchinson <virginiaehutchinson@gmail.com>
2021-07-09 23:12:34 -0600

    Replaced default controller with CampsController. Added route and Get.

---
+++ src/Controllers/CampsController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+
+        public CampsController(ICampRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsAsync();
+
+                CampModel[] models = 
+
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+    }
+}
