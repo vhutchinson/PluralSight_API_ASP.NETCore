22a81d614e13ac88160ba6373cdf660fb208fc90
vhutchinson <virginiaehutchinson@gmail.com>
2021-07-10 15:44:56 -0600

    Added Delete method.

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -137,5 +137,29 @@ namespace CoreCodeCamp.Controllers
 
             return BadRequest();
         }
+
+        [HttpDelete("{moniker}")]
+        public async Task<IActionResult> Delete(string moniker)
+        {
+            try
+            {
+                var oldCamp = await _repository.GetCampAsync(moniker);
+                if (oldCamp == null) return NotFound($"Could not find camp with moniker of {moniker}");
+
+                _repository.Delete(oldCamp);
+
+                if(await _repository.SaveChangesAsync())
+                {
+                    return Ok();
+                }
+            }
+            catch (Exception)
+            {
+
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
+            return BadRequest("Failed to delete the camp");
+        }
     }
 }
