46f913d9b4aaedd821671dbc634a4f15494ec603
vhutchinson <virginiaehutchinson@gmail.com>
2021-07-10 21:28:44 -0600

    Added versioning for 1.0 and 1.1 with different Get Camp methods.

--- src/Controllers/CampsController.cs
+++ src/Controllers/CampsController.cs
@@ -13,6 +13,8 @@ using System.Threading.Tasks;
 namespace CoreCodeCamp.Controllers
 {
     [Route("api/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiVersion("1.1")]
     [ApiController]
     public class CampsController : ControllerBase
     {
@@ -43,11 +45,30 @@ namespace CoreCodeCamp.Controllers
         }
 
         [HttpGet("{moniker}")]
+        [MapToApiVersion("1.0")]
         public async Task<ActionResult<CampModel>> Get(string moniker)
         {
             try
             {
                 var result = await _repository.GetCampAsync(moniker);
+
+                if (result == null) return NotFound();
+
+                return _mapper.Map<CampModel>(result);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+
+        [HttpGet("{moniker}")]
+        [MapToApiVersion("1.1")]
+        public async Task<ActionResult<CampModel>> Get11(string moniker)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(moniker, true);
 
                 if (result == null) return NotFound();
 
